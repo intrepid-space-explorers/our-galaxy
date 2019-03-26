613598679905cf4dfe1f2434ef8c499aced0a5cd
kcerbulis <kcerbulis@gmail.com>
2019-03-26 10:01:30 -0700

    started local storage

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 'use strict';
+
+var input_form = document.getElementById('star_data_form');
+var submit_button = document.getElementById('submit_button');
+submit_button.addEventListener('click', submit_handler);
+
+function submit_handler(event){
+  event.preventDefault();
+  console.log(event);
+}
