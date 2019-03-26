34aa790f92ec98c7e6c7f11597b3d94a61913756
mspake <morgana.spake@gmail.com>
2019-03-26 13:24:13 -0700

    galaxy.js all up to date with most current information

--- js/galaxy.js
+++ js/galaxy.js
@@ -31,7 +31,7 @@ var planet_types = [
   10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
   10, 10, 10, 10, 10, 10, 10, 10, 11, 11
 ];
-var star_data_image_url = ['../img/1_blackhole.jpg', '../img/2_neutron.jpg', '../img/3_white_dwarf.jpg', '../img/4_supernova_i.jpg', '../img/5_supernova_ii.jpg', '../img/6_red_giant.jpg', '../img/7_main_i.jpg', '../img/8_main_ii.jpg', '../img/9_main_iii.jpg', '../img/10_red_dwarf_1.jpg', '../img/11_red_dwarf_2.jpg', '../img/12_red_dwarf_3.jpg', '../img/13_blue_giant.jpg', '../img/14_dyson_sphere.jpg'];
+var star_data_image_url = ['../assets/star_pictures/s0_blackhole.png', '../assets/star_pictures/s1_neutron.png', '../assets/star_pictures/s2_white_dwarf.png', '../assets/star_pictures/s3_supernova_i.png', '../assets/star_pictures/s4_supernova_ii.png', '../assets/star_pictures/s5_red_giant.png', '../assets/star_pictures/s6_main_i.png', '../assets/star_pictures/s7_main_ii.png', '../assets/star_pictures/s8_main_iii.png', '../assets/star_pictures/s9_red_dwarf_1.png', '../assets/star_pictures/s10_red_dwarf_2.png', '../assets/star_pictures/s11_red_dwarf_3.png', '../assets/star_pictures/s12_blue_giant.png', '../assets/star_pictures/s13_dyson_sphere.png'];
 var star_data_name = ['Black Hole', 'Neutron Star', 'White Dwarf', 'Supernova-I', 'Supernova-II', 'Red Giant', 'Main Sequence I', 'Main Sequence II', 'Main Sequence III', 'Red Dwarf I', 'Red Dwarf II', 'Red Dwarf III', 'Blue Giant', 'Dyson Sphere'];
 var star_data_minage = [.1, 5, 7, .1, .1, .1, 2, 3, 4, 2, 2, 2, .1];
 var star_data_maxage = [13, 12, 13, .1, .1, .1, 6, 7, 8, 8, 10, 12, .5];
@@ -64,7 +64,7 @@ var intel_drake = JSON.parse(stringy_percent_of_inteligent_life);
 for (var i = 0; i < num_stars; i++) {
   var s_type = Math.floor(Math.random() * 13);
   new Build_star();
-  star_array[i].image_url = star_data_image_url[s_type];
+  star_array[i].image_url = s_type;
   star_array[i].type = s_type;
   star_array[i].name = star_data_name[s_type];
   star_array[i].age = ((star_data_maxage[s_type] - star_data_minage[s_type]) * Math.random()) + star_data_minage[s_type];
@@ -108,17 +108,45 @@ console.log(star_array);
 // localStorage.setItem('clicked_list_in_ls', clicked_list);
 
 //===============================
+// p5 Canvas
 //===============================
+var img;
+var background_img;
+var images = [];
 
-// function preload() {
-//   milky_way = loadImage('../img/Milky_Way-view2.jpg');
-// }
+function preload() {
+  for (var i = 0; i < star_data_image_url.length; i++) {
+    img = loadImage(star_data_image_url[i]);
+    images.push(img);
+  }
+  background_img = loadImage('../assets/star_pictures/Milky_Way-view3.jpg');
+
+}
 
 function setup() {
   var cnv = createCanvas(windowWidth, windowHeight, WEBGL);
-  // milky_way.width = window.innerWidth;
-  // milky_way.height = window.innerHeight;
   background(0);
+
+  push();
+  texture(background_img);
+  textureMode(NORMAL);
+  translate(0, 0, -1100);
+  plane(5000);
+  pop();
+
+  ambientMaterial(250);
+  directionalLight(255, 255, 255, 0, 1, -2);
+  noStroke();
+
+  for (var i in star_array) {
+    push();
+    translate(star_array[i].x, star_array[i].y, star_array[i].z);
+    texture(images[star_array[i].image_url]);
+    textureMode(NORMAL);
+    plane(30);
+    sphere(7);
+    pop();
+  }
 
 }
 
@@ -128,28 +156,19 @@ function windowResized() {
 }
 
 function draw() {
-  ambientMaterial(250);
-  directionalLight(255, 255, 255, 0, 1, -2);
-  noStroke();
 
-  for (var i in star_array) {
-    push();
-    translate(star_array[i].x, star_array[i].y, star_array[i].z);
-    // plane(100, 100);
-    sphere(7);
-    pop();
-  }
 }
 
 
 function randomized_coordinates() {
   var random_x = Math.floor(((Math.random() * (window.innerWidth - 20)) - (window.innerWidth / 2) + 10));
   var random_y = Math.floor(((Math.random() * (window.innerHeight - 20)) - (window.innerHeight / 2) + 10));
-  var random_z = Math.floor(Math.random() * -1000);
+  var random_psuedo_z = Math.floor(Math.random() * 7) + 3;
 
-  var coordinates = [random_x, random_y, random_z];
+  var coordinates = [random_x, random_y, random_psuedo_z];
   return coordinates;
 }
+
 
 
 //uncomment again when ready to play with the pop-up windows
