009c1d6899f64e983775337fcedcb6c84ec87ac0
kcerbulis <kcerbulis@gmail.com>
2019-03-26 10:20:56 -0700

    setting items in local storage

--- js/app.js
+++ js/app.js
@@ -6,5 +6,11 @@ submit_button.addEventListener('click', submit_handler);
 
 function submit_handler(event){
   event.preventDefault();
-  console.log(event);
+  var number_of_stars = Math.floor(document.getElementById('number_of_stars').value);
+  var percent_of_possible_life = document.getElementById('%of_pos_life_on_planet').value;
+  var percent_of_inteligent_life = document.getElementById('%of_intelegent_life').value;
+
+  localStorage.setItem('number_of_stars', number_of_stars);
+  localStorage.setItem('%of_pos_life_on_planet', percent_of_possible_life);
+  localStorage.setItem('%of_intelegent_life', percent_of_inteligent_life);
 }
