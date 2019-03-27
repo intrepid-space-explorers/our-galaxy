9a0382ba599c424549fb5eff9248be80dea537b7
Robert Tanner <RTannertech@gmail.com>
2019-03-27 16:57:50 -0700

    easter egg added

--- js/author.js
+++ js/author.js
@@ -5,7 +5,11 @@ var krisImg = document.getElementById('kris_img');
 var jonImg = document.getElementById('jon_img');
 var robertImg = document.getElementById('robert_img');
 
-function hover(robertImg){
-    Element.setAttribute('src', '../assets/background_pictures/m1-hear_no_evil.jpg')
-}
-hover();
+function hover(){
+  Image.setAttribute('src', '../assets/background_pictures/m1-hear_no_evil.jpg')
+};
+hover(robertImg);
+
+function unhover(){
+  Image.setAttribute('src', '../assets/background_pictures/a1-robert.jpg')
+};
