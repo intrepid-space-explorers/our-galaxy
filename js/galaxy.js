08370b112c02bd2d7e8688e2238760ab9934f8ac
Jon-Ramer <ramerj@att.net>
2019-03-27 13:51:22 -0700

    pix and fix

--- js/galaxy.js
+++ js/galaxy.js
@@ -32,8 +32,8 @@ var star_data_name = ['Black Hole', 'Neutron Star', 'White Dwarf', 'Supernova-I'
 var star_data_minage = [.1, 5, 7, .1, .1, .1, 2, 3, 4, 2, 2, 2, .1];
 var star_data_maxage = [13, 12, 13, .1, .1, .1, 6, 7, 8, 8, 10, 12, .5];
 var star_data_chance_planets = [0, .01, .05, 0, 0, .4, .9, .9, .9, .9, .9, .9, .05];
-var planet_data_image_url = ['../assets/star_pictures/p0_hot_giant.jpg', '../assets/star_pictures/p1_hot_rocky.jpg', '../assets/star_pictures/p2_rocky_dwarf.jpg', '../assets/star_pictures/p3_sub_terra.jpg', '../assets/star_pictures/p4_terra.jpg', '../assets/star_pictures/p5_super_terra.jpg', '../assets/star_pictures/p6_water_world.jpg', '../assets/star_pictures/p7_gas_giant_moon.jpg', '../assets/star_pictures/p8_gas_giant.jpg', '../assets/star_pictures/p9_super_jovian.jpg', '../assets/star_pictures/p10_ice_giant.jpg', '../assets/star_pictures/p11_ice_dwarf', '../assets/star_pictures/p12_dyson_sphere.jpg'];
-var planet_data_name = ['Hot Giant', 'Hot Rock', 'Rocky Dwarf', 'Sub Terra', 'Terra', 'Super Terra', 'Water World', 'Gas Giant Moon', 'Gas Giant', 'Super Jovian', 'Ice Giant', 'Ice Dwarf', 'Dyson Sphere'];
+var planet_data_image_url = ['../assets/star_pictures/p0_hot_giant.jpg', '../assets/star_pictures/p1_hot_rocky.jpg', '../assets/star_pictures/p2_rocky_dwarf.jpg', '../assets/star_pictures/p3_sub_terra.jpg', '../assets/star_pictures/p4_terra.jpg', '../assets/star_pictures/p5_super_terra.jpg', '../assets/star_pictures/p6_water_world.jpg', '../assets/star_pictures/p7_gas_giant_moon.jpg', '../assets/star_pictures/p8_gas_giant.jpg', '../assets/star_pictures/p9_super_jovian.jpg', '../assets/star_pictures/p10_ice_giant.jpg', '../assets/star_pictures/p11_ice_dwarf.jpg', '../assets/star_pictures/p12_dyson_sphere.jpg'];
+var planet_data_name = ['Hot Giant', 'Hot Rocky', 'Rocky Dwarf', 'Sub Terra', 'Terra', 'Super Terra', 'Water World', 'Gas Giant Moon', 'Gas Giant', 'Super Jovian', 'Ice Giant', 'Ice Dwarf', 'Dyson Sphere'];
 var planet_data_chance_life = [0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1];
 var total_life_count = 0;
 var total_intel_count = 0;
@@ -100,7 +100,6 @@ Build_star.prototype.populate_with_data = function() {
   var data_list = document.createElement('ul');
 
   var name = document.createElement('li');
-  // console.log(data);
   name.textContent = `Type: ${this.name}`;
   data_list.appendChild(name);
 
@@ -127,54 +126,57 @@ Build_star.prototype.populate_with_data = function() {
 for (var i = 0; i < num_stars; i++) {
   // 14 types of stars in database, pick random number 0 to 99, set s-type to star of that number
   var chance = Math.floor(Math.random() * 99);
-  var chance2 = Math.floor(Math.random() * 99); // easter egg time, add dyson sphere
+  var chance2 = Math.floor(Math.random() * 99); // easter egg time!  add dyson sphere
   if (chance === 99 && chance2 === 99) {
     s_type = 100;
   }
   var s_type = star_types[chance];
   new Build_star();
-  star_array[i].image_url = s_type;
+  star_array[i].image_url = star_data_image_url[s_type];
   star_array[i].type = s_type;
   star_array[i].name = star_data_name[s_type];
-  star_array[i].age = ((star_data_maxage[s_type] - star_data_minage[s_type]) * Math.random()) + star_data_minage[s_type];
+  star_array[i].age = (Math.floor((star_data_maxage[s_type] - star_data_minage[s_type]) * Math.random() * 1000)) / 1000 + star_data_minage[s_type];
   star_array[i].x = randomized_coordinates()[0];
   star_array[i].y = randomized_coordinates()[1];
   star_array[i].z = randomized_coordinates()[2];
   var chance = Math.random();  // set random chance for if star has planets
   if (chance < star_data_chance_planets[s_type]) {
     star_array[i].has_planets = true;
-    var num_planets = Math.floor(Math.random() * 9) + 1;  
-    // max number of possible planets in solar system is 10
+    var num_planets = Math.floor(Math.random() * 8) + 2;  
+    // max number of possible planets in solar system is arbitrarily set to 2 to 10
     for (var j = 0; j < num_planets; j++) {
       var k = 0;
-      while (k = 0) {
+      while (k === 0) {
         var chance = Math.floor(Math.random() * 99);
         // 12 types of planets in database, random pick of 0 to 99, set p-type to planet of that number
         var p_type = planet_types[chance];
         // conditionals to put hot planets by star and cold planets away from it
-        if (((j === 1 && num_planets > 1) && (p_type === 10 || p_type === 11)) || ((j === num_planets || j === (num_planets - 1)) && (p_type === 0 || p_type === 1))) {
+        if (
+          ((j === 0 && num_planets > 1) && (p_type === 10 || p_type === 11)) ||
+          ((j === num_planets || j === (num_planets - 1)) && (p_type === 0 || p_type === 1)) ||
+          (j > 2 && (p_type === 0 || p_type === 1)) ||
+          (j < num_planets - 3) && (p_type === 10 || p_type === 11)
+          ) {
           k = 0;
         } else {
-          k = 1 
+          k = 1;
         }
       }
       new Build_planet(i);
       star_array[i].planets[j].image_url = planet_data_image_url[p_type];
       star_array[i].planets[j].type = p_type;
       star_array[i].planets[j].name = planet_data_name[p_type];
-      star_array[i].planets[j].age = star_array[i].age;
       var chance_of_life = Math.random() / life_drake;  // set random chance if planet has life
       if (chance_of_life < planet_data_chance_life[p_type]) {
         star_array[i].life = 1;
         total_life_count++;
-      }
-      var chance_of_intel = Math.random() / intel_drake;  // set random chance if life is intelligent
-      if (star_array[i].age > 4) {
-        if (chance_of_intel > 1) {
-          star_array[i].intel = 1; 
-          total_intel_count++;
+        var chance_of_intel = Math.random() / intel_drake;  // set random chance if life is intelligent
+        if (star_array[i].age > 4 && chance_of_intel > 1) {
+            star_array[i].intel = 1; 
+            total_intel_count++;
         }
-      } //  dyson sphere logic
+      }
+      //  dyson sphere logic
       if (star_array[i].type === 100) {
         star_array[i].planets[j].image_url = planet_data_image_url[12];
         star_array[i].planets[j].type = 12;
@@ -187,6 +189,7 @@ for (var i = 0; i < num_stars; i++) {
     }
   }
 }
+debugger;
 
 // ================================================
 // p5 Canvas area
